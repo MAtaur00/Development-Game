9911c42e6afc627b322b49bddac172e442ba3a8b
Aitor <aitorvelez@hotmail.com>
2018-11-11 13:36:33 +0100

    Tmer modules added

--- DevGame/Motor2D/j1App.cpp
+++ DevGame/Motor2D/j1App.cpp
@@ -17,8 +17,13 @@
 // Constructor
 j1App::j1App(int argc, char* args[]) : argc(argc), args(args)
 {
+
+	PERF_START(ptimer);
+
 	frames = 0;
 	want_to_save = want_to_load = false;
+
+
 
 	input = new j1Input();
 	win = new j1Window();
@@ -43,6 +48,8 @@ j1App::j1App(int argc, char* args[]) : argc(argc), args(args)
 
 	// render last to swap buffer
 	AddModule(render);
+
+	PERF_PEEK(ptimer);
 }
 
 // Destructor
@@ -70,6 +77,8 @@ void j1App::AddModule(j1Module* module)
 bool j1App::Awake()
 {
 
+	PERF_START(ptimer);
+
 	bool ret = false;
 		
 	config = LoadConfig(config_file);
@@ -95,12 +104,16 @@ bool j1App::Awake()
 		}
 	}
 
+	PERF_PEEK(ptimer);
+
 	return ret;
 }
 
 // Called before the first frame
 bool j1App::Start()
 {
+
+	PERF_START(ptimer);
 	bool ret = true;
 	p2List_item<j1Module*>* item;
 	item = modules.start;
@@ -110,7 +123,8 @@ bool j1App::Start()
 		ret = item->data->Start();
 		item = item->next;
 	}
-
+	
+	PERF_PEEK(ptimer);
 	return ret;
 }
 
@@ -164,6 +178,29 @@ void j1App::FinishUpdate()
 
 	if(want_to_load == true)
 		LoadGameNow();
+
+
+	// Framerate calculations --
+
+	if (last_sec_frame_time.Read() > 1000)
+	{
+		last_sec_frame_time.Start();
+		prev_last_sec_frame_count = last_sec_frame_count;
+		last_sec_frame_count = 0;
+	}
+
+	float avg_fps = float(frame_count) / startup_time.ReadSec();
+	float seconds_since_startup = startup_time.ReadSec();
+	uint32 last_frame_ms = frame_time.Read();
+	uint32 frames_on_last_update = prev_last_sec_frame_count;
+
+	static char title[256];
+	sprintf_s(title, 256, "Av.FPS: %.2f Last Frame Ms: %02u Last sec frames: %i  Time since startup: %.3f Frame Count: %lu ",
+		avg_fps, last_frame_ms, frames_on_last_update, seconds_since_startup, frame_count);
+	App->win->SetTitle(title);
+
+	uint32 cap = 1 / (float)framelimit;
+	SDL_Delay(cap);
 }
 
 // Call modules before each loop iteration
