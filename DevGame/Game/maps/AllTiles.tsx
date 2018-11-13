3b13df17f8152ae3447db5ed961386f52433990f
AitorVelez <aitorvelez@hotmail.com>
2018-11-13 13:29:02 +0100

    Added new Level2 tmx

---
+++ DevGame/Game/maps/AllTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="AllTiles" tilewidth="32" tileheight="32" tilecount="3082" columns="67">
+ <image source="AllTiles.png" width="2175" height="1472"/>
+</tileset>
